9770f9fdab77ba7c40adcf131de53d88e8275683
Josh Deprez <josh.deprez@gmail.com>
2021-10-19 17:51:23 +1100

    WIP

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.17
 
 require (
 	github.com/alecthomas/participle v0.7.1
+	github.com/alecthomas/participle/v2 v2.0.0-alpha7
 	google.golang.org/protobuf v1.27.1
 )
--- strings.go
+++ strings.go
@@ -22,7 +22,8 @@ import (
 	"strings"
 	"unicode"
 
-	"github.com/alecthomas/participle"
+	"github.com/alecthomas/participle/v2"
+	"github.com/alecthomas/participle/v2/lexer"
 )
 
 // StringTableRow contains all the information from one row in a string table.
@@ -86,27 +87,91 @@ func (t *StringTable) Render(line Line) (string, error) {
 	return t.render(row.Text, line.Substitutions)
 }
 
+var _ = lexer.MustStateful(lexer.Rules{
+	"Root": {
+		// A line is a funny kind of string (no surrounding quotes, so quotes
+		// need not be escaped.)
+		{Name: "Escaped", Pattern: `\\[{}\[\]]`, Action: nil},
+		{Name: "Func", Pattern: `\[`, Action: lexer.Push("Func")},
+		{Name: "Subst", Pattern: `{`, Action: lexer.Push("Subst")},
+		{Name: "Char", Pattern: `[^{\[]+`, Action: nil},
+	},
+	"Func": {
+		{Name: "Whitespace", Pattern: `\s+`, Action: nil},
+		{Name: "String", Pattern: `"`, Action: lexer.Push("String")},
+		{Name: "Ident", Pattern: `\w+`, Action: nil},
+		{Name: "FuncEnd", Pattern: `\]`, Action: lexer.Pop()},
+	},
+	"Subst": {
+		{Name: "Index", Pattern: `\d+`, Action: nil},
+		{Name: "SubstEnd", Pattern: `}`, Action: lexer.Pop()},
+	},
+	"String": {
+		{Name: "Escaped", Pattern: `\\[{}\[\]"]`, Action: nil},
+		{Name: "Percent", Pattern: `%`, Action: nil},
+		{Name: "StringEnd", Pattern: `"`, Action: lexer.Pop()},
+		{Name: "Func", Pattern: `\[`, Action: lexer.Push("Func")},
+		{Name: "Subst", Pattern: `{`, Action: lexer.Push("Subst")},
+		{Name: "Char", Pattern: `[^{\[%]+`, Action: nil},
+	},
+})
+
+type parsedLine struct {
+	Fragments []*lineFragment `parser:"@@*"`
+}
+
+type lineFragment struct {
+	Escaped string       `@Escaped`
+	Func    *parsedFunc  `| "[" @@ "]"`
+	Subst   *parsedSubst `| "{" @@ "}"`
+	Text    string       `| @Char`
+}
+
+type parsedFunc struct {
+	Name  string        `@Ident`
+	Input *parsedString `"\"" @@ "\""`
+	Opts  []*parsedOpt  `@@+`
+}
+
+type parsedString struct {
+	Escaped string       `@Escaped`
+	Percent string       `| @Percent`
+	Func    *parsedFunc  `| "[" @@ "]"`
+	Subst   *parsedSubst `| "{" @@ "}"`
+	Text    string       `| @Char`
+}
+
+type parsedSubst struct {
+	Index string `@Index`
+}
+
+type parsedOpt struct {
+	Key   string        `@Ident "="`
+	Value *parsedString `@@`
+}
+
 func (t *StringTable) render(text string, substs []string) (string, error) {
-	// Do substitutions and format functions in one stateful loop.
+	// Do substitutions and format functions in one big stateful loop.
 	var sb strings.Builder
+
+	// parser states
 	const (
 		raw = iota
 		subst
 		fmtf
 		fmtfstr
+		fmtfsub
+		eatspace
 	)
 	escape := false // true means next rune is escaped
 	stack, state := []int{}, raw
-	push := func(st int) {
-		stack, state = append(stack, state), st
-	}
-	pop := func() {
-		stack, state = stack[:len(stack)-1], stack[len(stack)-1]
-	}
-	substIdx := 0
-	var ffstart int
+	push := func(st int) { stack, state = append(stack, state), st }
+	pop := func() { stack, state = stack[:len(stack)-1], stack[len(stack)-1] }
+	var substIdx int
+	var fb strings.Builder
 
-	for i, r := range text {
+	for _, r := range text {
+	loopStart:
 		switch state {
 		case raw:
 			if escape {
@@ -114,7 +179,8 @@ func (t *StringTable) render(text string, substs []string) (string, error) {
 				case '[', ']', '{', '}':
 					// escape sequences in raw state are \[, \], \{, and \}
 				default:
-					// this char is not part of an escape sequence
+					// this char is not part of an escape sequence, or the
+					// sequence is otherwise passed through
 					sb.WriteRune('\\')
 				}
 				sb.WriteRune(r)
@@ -125,9 +191,10 @@ func (t *StringTable) render(text string, substs []string) (string, error) {
 			case '\\':
 				escape = true
 			case '[':
-				ffstart = i
 				push(fmtf)
+				push(eatspace)
 			case '{':
+				substIdx = 0
 				push(subst)
 			default:
 				sb.WriteRune(r)
@@ -149,22 +216,16 @@ func (t *StringTable) render(text string, substs []string) (string, error) {
 			}
 
 		case fmtf:
-			// find the range of the format function within the string
-			if escape {
-				escape = false
-				break
-			}
 			switch r {
-			case '\\':
-				escape = true
 			case '"':
 				// a string within the format function
+				fb.WriteRune(r)
 				push(fmtfstr)
 			case ']':
 				// parse and evaluate the function
 				ff := new(fmtFunc)
-				if err := funcParser.ParseString(text[ffstart+1:i], ff); err != nil {
-					return "", err
+				if err := funcParser.ParseString("", fb.String(), ff); err != nil {
+					return "", fmt.Errorf("parsing format function %q: %w", fb.String(), err)
 				}
 				fr, err := ff.render(substs, t.langCode)
 				if err != nil {
@@ -172,22 +233,69 @@ func (t *StringTable) render(text string, substs []string) (string, error) {
 				}
 				sb.WriteString(fr)
 				pop()
+			default:
+				fb.WriteRune(r)
 			}
 
 		case fmtfstr:
 			// a string within function
 			if escape {
+				switch r {
+				case '{', '}':
+					// escape sequences in this state are \{, \}
+				default:
+					// this char is not part of an escape sequence, or the
+					// sequence is passed through
+					fb.WriteRune('\\')
+				}
+				fb.WriteRune(r)
 				escape = false
 				break
 			}
 			switch r {
 			case '\\':
 				escape = true
+			case '{':
+				// substitution within string within function
+				substIdx = 0
+				push(fmtfsub)
 			case '"':
 				// end of string
+				fb.WriteRune(r)
 				pop()
+			default:
+				fb.WriteRune(r)
 			}
+
+		case fmtfsub:
+			// substitution within a string within a function
+			switch {
+			case unicode.IsDigit(r):
+				substIdx *= 10
+				substIdx += int(r - '0')
+			case r == '}':
+				if substIdx < 0 || substIdx >= len(substs) {
+					return "", fmt.Errorf("substitution index %d out of range [0, %d)", substIdx, len(substs))
+				}
+				fb.WriteString(substs[substIdx])
+				pop()
+			default:
+				return "", fmt.Errorf("invalid rune %c in substitution", r)
+			}
+
+		case eatspace:
+			if !unicode.IsSpace(r) {
+				pop()
+				// Reprocess r from the start of the loop body.
+				goto loopStart
+			}
+
+		default:
+			return "", fmt.Errorf("unknown parser state %d", state)
 		}
+	}
+	if len(stack) != 0 || state != raw {
+		return "", fmt.Errorf("parser ended line in bad state %d [stack:%v]", state, stack)
 	}
 	if escape {
 		// trailing backslash is not an escape sequence
@@ -208,39 +316,82 @@ type fmtFunc struct {
 var funcParser = participle.MustBuild(&fmtFunc{})
 
 func (f *fmtFunc) render(substs []string, langCode string) (string, error) {
-	// is the input a substitution?
-	if strings.HasPrefix(f.Input, "{") && strings.HasSuffix(f.Input, "}") {
-		n, err := strconv.Atoi(strings.Trim(f.Input, "{}"))
-		if err != nil {
-			return "", fmt.Errorf("invalid substitution: %w", err)
-		}
-		if n < 0 || n >= len(substs) {
-			return "", fmt.Errorf("substitution index %d out of range [0, %d)", n, len(substs))
-		}
-		f.Input = substs[n]
+	in, err := strconv.Unquote(f.Input)
+	if err != nil {
+		return "", err
 	}
 
 	switch f.Name {
 	case "select":
-		// input chooses which value to interpolate directly
+		// input chooses which value to interpolate
 		for _, kv := range f.Opts {
-			if kv.K == f.Input {
-				return kv.V, nil
+			if kv.K == in {
+				return strconv.Unquote(kv.V)
 			}
 		}
-		return "", fmt.Errorf("key %q not found in %v", f.Input, f.Opts)
+		return "", fmt.Errorf("key %q not found in %v", in, f.Opts)
 
 	case "plural":
 		// input is itself a number to feed through CLDR to find the
 		// pluralisation case
-		// TODO
+		n, err := strconv.Atoi(in)
+		if err != nil {
+			return "", fmt.Errorf("format function input not an integer: %w", err)
+		}
+		// TODO: use CLDR
+		switch n {
+		case 1:
+			for _, kv := range f.Opts {
+				if kv.K == "one" {
+					return strconv.Unquote(kv.V)
+				}
+			}
+		default:
+			for _, kv := range f.Opts {
+				if kv.K == "other" {
+					return strconv.Unquote(kv.V)
+				}
+			}
+		}
 
 	case "ordinal":
 		// input is itself a number to feed through CLDR to find the
 		// ordinal case
 		// TODO
+		n, err := strconv.Atoi(in)
+		if err != nil {
+			return "", fmt.Errorf("format function input not an integer: %w", err)
+		}
+		// TODO: use CLDR
+		switch n {
+		case 1:
+			for _, kv := range f.Opts {
+				if kv.K == "one" {
+					return strconv.Unquote(kv.V)
+				}
+			}
 
+		case 2:
+			for _, kv := range f.Opts {
+				if kv.K == "two" {
+					return strconv.Unquote(kv.V)
+				}
+			}
+
+		case 3:
+			for _, kv := range f.Opts {
+				if kv.K == "few" {
+					return strconv.Unquote(kv.V)
+				}
+			}
+
+		default:
+			for _, kv := range f.Opts {
+				if kv.K == "other" {
+					return strconv.Unquote(kv.V)
+				}
+			}
+		}
 	}
 	return "", fmt.Errorf("unknown format function %q", f.Name)
-
 }
